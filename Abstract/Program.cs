f20a9114d6086156de53b5f41bd2545253cf9daa
Aghababayev <aghababayev52@gmail.com>
2022-04-23 13:50:48 +0400

    this is C# training

---
+++ Abstract/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string a = "test";
+            int b  ;
+            int.TryParse(a, out  b);
+            Console.WriteLine(b);
+            Console.ReadKey();
+        }
+
+       
+        
+    }
+
+}
---
+++ test console/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_console
+{
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Eded {x.Counter()} Reqemlidir");
+           
+            Console.ReadKey();
+        }
+
+    
+
+    }
+   static class Extention
+    {
+        public static int Counter(this int num)
+        {
+          int count = 0;
+            while (num!=0)
+            {
+                count++;
+                num = num / 10;
+            }
+            return count;
+        }
+    
+    }
+}
+ 
+
